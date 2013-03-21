38e7f43b9845f9128479f0e639d6c08223d4ee67
Hendrik <lammers@gmail.com>
2013-03-21 19:47:23 +0100

    solved problem 4

--- js/problem4.js
+++ js/problem4.js
@@ -1,7 +1,7 @@
 /**
  * Largest palindrome product
  *
- * A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 99.
+ * A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 * 99.
  *
  * Find the largest palindrome made from the product of two 3-digit numbers.
  */
@@ -9,12 +9,31 @@
 (function() {
 	'use strict';
 
+	function searchLargestPalindrome() {
+		var largestPalindrome = 0,
+			startTime = new Date().getTime(),
+			elapsedTime;
+
+		for (var i = 999; i > 99; i--) {
+			for (var y = 999; y > 99; y--) {
+				var result = i * y;
+				if (isPalindrome(result) && result > largestPalindrome) {
+					largestPalindrome = result;
+				}
+			}
+		}
+
+		elapsedTime = (new Date().getTime() - startTime) / 1000;
+		console.log(largestPalindrome);
+		console.log('Time elapsed: ' + elapsedTime + ' seconds');
+	}
+
 	/**
 	 * Check's whether a number is palindromic or not
 	 * @param  {Number}  num Value to check
 	 * @return {Boolean}     is it a palindromic number?
 	 */
-	function isPalindromic (num) {
+	function isPalindrome (num) {
 		// First break number into array with individual numbers
 		var arr = ''.split.call(num, ''),
 			last = arr.length - 1;
@@ -30,6 +49,6 @@
 		return true;
 	}
 
-	
+	searchLargestPalindrome();
 
 }());
