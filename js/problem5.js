ec4bfe0ebe06bde1d534c76e17a9b54ba3e8b0ee
Hendrik <lammers@gmail.com>
2013-03-21 21:18:34 +0100

    Solved problem 5

--- js/problem5.js
+++ js/problem5.js
@@ -8,6 +8,31 @@
 (function() {
 	'use strict';
 
+	function findSmallestMultiple () {
+		var outcome = false,
+			count = 1,
+			startTime = new Date().getTime();
 
+		while (outcome === false) {
+			for (var i = 2; i <= 20; i++) {
+				if (count % i === 0) {
+					outcome = true;
+					// continue;
+				} else {
+					outcome = false;
+					break;
+				}
+			}
+
+			if (outcome === true) {
+				console.log(count);		// Result
+				console.log('Time elapsed: ' + ((new Date().getTime() - startTime) / 1000) + ' seconds');
+			}
+
+			count++;
+		}
+	}
+
+	findSmallestMultiple();
 
 }());
