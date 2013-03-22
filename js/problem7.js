fe3b23e79354d42fd5b02f06d840a65d7eeeb3e0
Hendrik <lammers@gmail.com>
2013-03-22 17:14:20 +0100

    Soled problem 7

--- js/problem7.js
+++ js/problem7.js
@@ -9,6 +9,53 @@
 (function() {
 	'use strict';
 
-	
-	
+	/**
+	 * Use isPrimeNumber to check whether a number is a Prime number or not.
+	 * @param  {Number}  value The number to check
+	 * @return {Boolean}       Is the given value a Prime Number or not.
+	 */
+	function isPrimeNumber (value) {
+		var max = Math.sqrt(value);
+		// Do some checks to prevent the loop from running for numbers that are obvious
+		if (isNaN(value) || !isFinite(value) || value % 1 || value < 2) {
+			return false;
+		}
+		if (value % 2 === 0) {
+			return (value === 2);
+		}
+		if (value % 3 === 0) {
+			return (value === 3);
+		}
+
+		// Go through posible divisions
+		for (var i = 5; i <= max; i += 6) {
+			if (value % i === 0) {
+				return false;
+			}    
+			if (value % (i + 2) === 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	function findPrimeNumber(index) {
+		var primeCount = 0,
+			counter = 0,
+			startTime = new Date().getTime();
+
+		while (primeCount < index) {
+			counter++;
+
+			if (isPrimeNumber(counter)) {
+				primeCount++;
+			}
+		}
+
+		console.log(counter);	// Result
+		console.log('Time elapsed: ' + (new Date().getTime() - startTime) / 1000 + ' seconds');
+	}
+
+	findPrimeNumber(10001);
+
 }());
