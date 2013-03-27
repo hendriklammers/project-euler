90e2a5ec9a6f7df9cc57c860898bd8df478b5842
Hendrik <lammers@gmail.com>
2013-03-27 11:30:56 +0100

    Added test suite

---
+++ test/spec/findsequence_spec.js
@@ -0,0 +1,6 @@
+describe("Find a sequence in a large number", function() {
+  it("should have a test function", function() {
+    expect(test()).toBeDefined();
+    expect(test()).toEqual('testing is fun!');
+  });
+});
