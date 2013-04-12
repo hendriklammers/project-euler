6383a3bd4933cac133406765f7d46edb918c6b75
Hendrik <lammers@gmail.com>
2013-04-12 15:55:09 +0200

    Added problem 11

---
+++ script/problem11.js
@@ -0,0 +1,18 @@
+/**
+ * Largest product in a grid
+ *
+ * What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 2020 grid?
+ *
+ * http://projecteuler.net/problem=11
+ */
+
+(function() {
+	'use strict';
+
+	var findSolution = function() {
+
+	};
+
+	findSolution();	
+	
+}());
