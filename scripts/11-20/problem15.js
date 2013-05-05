63cbb246d7e2aa21cf8d334dc5ff41938978e4a9
Hendrik <lammers@gmail.com>
2013-05-05 19:58:01 +0200

    Added todo

--- scripts/11-20/problem15.js
+++ scripts/11-20/problem15.js
@@ -10,6 +10,7 @@ How many such routes are there through a 20*20 grid?
  * http://projecteuler.net/problem=15
  */
 // TODO: Create canvas animation of the routes, just for fun
+// TODO: Try math solution?
 var problem15 = (function() {
 	'use strict';
 
