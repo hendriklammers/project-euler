fa07a7fe8bb7bf816729cc0f7da1a64b06e2fc08
Hendrik Lammers <lammers@gmail.com>
2013-05-20 17:36:34 +0200

    Solved problem by starting at the bottom of the triangle

--- scripts/11-20/problem18.js
+++ scripts/11-20/problem18.js
@@ -20,15 +20,23 @@ var problem18 = (function() {
 	'use strict';
     //TODO: Solve problem by starting at the bottom of the triangle
     
-	var triangle = [],
+	var triangleData = [],
+	
+	    exampleTriangle = [
+	        [3],
+            [7, 4],
+            [2, 4, 6],
+            [8, 5, 9, 3]
+	    ],
+	    result,
 	
 	    findSolution = function() {
 			var startTime = new Date().getTime();
 			
-			// Bruteforce using recursion
-			var result = findPath(0, 0);
+			//bruteForceSolution();
 			
-			// Show result
+			// Smarter solution
+			result = findMaximumPath(triangleData);
 			console.log(result);
 			
 			// Show the elapsed time
@@ -45,27 +53,68 @@ var problem18 = (function() {
 		dataLoadHandler = function() {
 		    var data = this.responseText;
 		    
-		    triangle = data.split('\n');
+		    triangleData = data.split('\n');
 		    
-		    for (var i in triangle) {
-		        triangle[i] = triangle[i].split(' ');
+		    for (var i in triangleData) {
+		        triangleData[i] = triangleData[i].split(' ');
 		    }
 		    
 		    findSolution();
 		},
 		
+		bruteForceSolution = function() {
+		    // Brute force using recursion
+			var result = findPath(0, 0);
+			
+			// Show result
+			console.log(result);
+		},
+		
 		// Recursive method that returns the biggest sum path
 		findPath = function(x, y) {
 		    // All numbers in the array are still strings so parse it first..
-		    var currentNum = parseInt(triangle[y][x], 10);
+		    var currentNum = parseInt(triangleData[y][x], 10);
 		    
 		    // When at the bottom of the triangle
-		    if (y === triangle.length - 1) {
+		    if (y === triangleData.length - 1) {
 		        return currentNum;
 		    } else {
 		        // Get biggest path from the 2 numbers on the row below this one.
 		        return Math.max(currentNum + findPath(x, y + 1), currentNum + findPath(x + 1, y + 1));
 		    }
+		},
+		
+		findMaximumPath = function(triangle) {
+		    // Temporary array with highest of current row
+		    var tempRow = [],
+		        copy = [],
+		        rowLength;
+		    
+		    // Go over each row
+		    for (var i = triangle.length - 1; i >= 0; i--) {
+		        rowLength = triangle[i].length;
+		        copy = [];
+		        
+		        for (var k = 0; k < rowLength; k++) {
+		            if (tempRow.length > 0) {
+		                copy[k] = parseInt(triangle[i][k], 10) + tempRow[k];
+		            } else {
+		                copy[k] = parseInt(triangle[i][k], 10);
+		            }
+		        }
+		        console.log(copy);
+
+		        // Start with empty row
+		        tempRow = [];
+		        // Only needed when not at the top row
+		        if (i > 0) {
+                    for (var j = 0; j < rowLength - 1; j++) {
+                        tempRow.push(Math.max(copy[j], copy[j + 1]));
+                    }
+		        }
+		    }
+		    
+		    return copy[0];
 		};
 
 	return {
