1d6d66e727cc5526cdd9b0f4e2942f5c3a1ef7b5
Hendrik Lammers <lammers@gmail.com>
2013-05-20 23:42:31 +0200

    Found solution for problem 19

--- scripts/11-20/problem19.js
+++ scripts/11-20/problem19.js
@@ -21,8 +21,55 @@
 var problem19 = (function() {
     'use strict';
 
-    var findSolution = function() {
+    var numberOfDays = function(year) {
+            var feb = 28;
             
+            if (year % 4 === 0) {
+                feb = 29;
+                
+                if (year % 100 === 0) {
+                    feb = 28;
+                    
+                    if (year % 400 === 0) {
+                        feb = 29;
+                    }
+                }
+            }
+            
+            return [31, feb, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        },
+    
+        findSolution = function() {
+            var numYears = 100,
+                currentDay = 0, // Start on monday
+                sundays = 0;
+                
+            // For each year, but start 1 year earlier
+            for (var i = 0; i <= numYears; i++) {
+                var days = numberOfDays(1900 + i);
+                
+                // For each month
+                for (var j = 0; j < 12; j++) {
+                    
+                    // Every day
+                    for (var k = 0; k < days[j]; k++) {
+                        
+                        // Don't count sundays in the first year
+                        if (i > 0) {
+                            // Check if current day is sunday and if it's the first of the month
+                            if (currentDay === 6 && k === 0) {
+                                sundays++;
+                            }
+                        }
+                        
+                        // Set next day
+                        currentDay = currentDay < 6 ? currentDay + 1 : 0;
+                    }
+                }
+            }
+            
+            // Result
+            console.log(sundays);
         };
 
     return {
